835d16e32e053d8479a3a26c6c69db547549cd69
jeong5575 <38935274+jeong5575@users.noreply.github.com>
2023-07-04 17:09:19 +0900

    Time: 106 ms (93.48%), Space: 21.2 MB (58.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,7 +5,6 @@ class Solution:
         for i in nums:
             count[i] +=1
             
-            
-        temp = sorted(count.items(), key = lambda x:x[1])[-k:]
-        return [ x[0] for x  in temp] 
+
+        return [ x[0] for x  in sorted(count.items(), key = lambda x:x[1])[-k:]] 
         
